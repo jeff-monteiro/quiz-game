b62fee4e8526ab5f564283bde9e945c37e2440bd
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2026-01-31 18:57:08 -0300

    feat:runMethodApplication

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -16,7 +17,7 @@ type Question struct {
 
 type GameState struct {
 	Name      string
-	Points    string
+	Points    int
 	Questions []Question
 }
 
@@ -53,12 +54,12 @@ func (g *GameState) ProccessCSV() {
 	}
 
 	for index, record := range records {
-		fmt.Println(index, record)
 		if index > 0 {
+			correctAnswer, _ := toInt(record[5])
 			question := Question{
 				Text:    record[0],
 				Options: record[1:5],
-				Answer:  toInt(record[5]),
+				Answer:  correctAnswer,
 			}
 
 			g.Questions = append(g.Questions, question)
@@ -68,20 +69,58 @@ func (g *GameState) ProccessCSV() {
 
 }
 
+func (g *GameState) Run() {
+	for index, question := range g.Questions {
+		fmt.Printf("\033[35m %d. %s \033[0m\n", index+1, question.Text)
+
+		for j, option := range question.Options {
+			fmt.Printf("[%d] %s\n", j+1, option)
+		}
+
+		fmt.Println("Digite uma alternativa")
+
+		var answer int
+		var err error
+
+		for {
+			reader := bufio.NewReader(os.Stdin)
+			read, _ := reader.ReadString('\n')
+
+			answer, err = toInt(read[:len(read)-1])
+
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+			break
+		}
+		fmt.Println(answer)
+
+		if answer == question.Answer {
+			fmt.Println("Congratulations, Right answer!")
+			g.Points += 10
+		} else {
+			fmt.Println("Ops! You were wrong!")
+			fmt.Println("<><><><><><><><><><><><><><><><><><><>")
+		}
+	}
+
+}
+
 func main() {
 	game := &GameState{}
-	go game.ProccessCSV()
-	game.Init()
+	game.ProccessCSV()
+	//game.Init()
+	game.Run()
 
-	fmt.Println(game)
 }
 
 // Function to make conversion between string and int types
-func toInt(s string) int {
+func toInt(s string) (int, error) {
 	i, err := strconv.Atoi(s)
 	if err != nil {
-		panic("Erro na conversao de tipos")
+		return 0, errors.New("Not allowed insert charactere different of a number!")
 	}
 
-	return i
+	return i, nil
 }
